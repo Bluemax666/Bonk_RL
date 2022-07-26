d024023a35de9cc453d33883c96435a5b6201014
Bluemax666 <44173031+Bluemax666@users.noreply.github.com>
2022-07-26 22:25:46 +0200

    Update and rename trainBonkSB3.py to train.py

--- train.py
+++ train.py
@@ -26,12 +26,11 @@ time.sleep(3)
 env = Env(update_opponent_every=400)
 
 
-# policy_kwargs = dict(n_critics=2, n_quantiles=25, net_arch=dict(pi=[256, 256], qf=[256, 256, 256]))
-# model = TQC("MlpPolicy", env, gamma=0.9925, top_quantiles_to_drop_per_net=2, batch_size=192,
-#             use_sde=True, policy_kwargs=policy_kwargs, verbose=1, device='cpu')
+policy_kwargs = dict(n_critics=2, n_quantiles=25, net_arch=dict(pi=[256, 256], qf=[256, 256, 256]))
+model = TQC("MlpPolicy", env, gamma=0.9925, top_quantiles_to_drop_per_net=2, batch_size=192,
+            use_sde=True, policy_kwargs=policy_kwargs, verbose=1, device='cpu')
 
-model = TQC.load("runs/bonk02", env=env)
 env.setModel(model)
 
 model.learn(total_timesteps=600_000, log_interval=25, callback=custom_callback)
-model.save("runs/bonk03")
+model.save("runs/bonk01")
