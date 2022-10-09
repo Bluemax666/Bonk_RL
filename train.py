f8437d9c98e855dc199f2393ddb0f02c96033fa8
Bluemax666 <44173031+Bluemax666@users.noreply.github.com>
2022-10-09 20:08:17 +0200

    Update train.py

--- train.py
+++ train.py
@@ -6,16 +6,18 @@ Created on Sun Jul 24 02:49:45 2022
 """
 from bonk_env import Env
 from sb3_contrib import TQC
+from stable_baselines3.common.callbacks import BaseCallback
+import keyboard
 import time
 
 time.sleep(3)
-env = Env()
+env = Env(save_opponent_every=1000, n_start_opponent=39)
 
-policy_kwargs = dict(n_critics=2, n_quantiles=25, net_arch=dict(pi=[256, 256], qf=[256, 256, 256]))
-model = TQC("MlpPolicy", env, gamma=0.9925, top_quantiles_to_drop_per_net=2, batch_size=192,
-            use_sde=True, policy_kwargs=policy_kwargs, verbose=1)
+policy_kwargs = dict(n_critics=3, n_quantiles=25, net_arch=dict(pi=[256, 256, 256], qf=[512, 512, 512]))
+model = TQC("MlpPolicy", env, gamma=0.995, top_quantiles_to_drop_per_net=2, batch_size=256,
+            use_sde=True, policy_kwargs=policy_kwargs, verbose=1, device='cuda')
 
 env.setModel(model)
 
-model.learn(total_timesteps=600_000, log_interval=25)
+model.learn(total_timesteps=2_000_000, log_interval=25)
 model.save("runs/bonk01")
